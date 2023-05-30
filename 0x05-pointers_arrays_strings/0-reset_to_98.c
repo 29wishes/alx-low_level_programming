65c0f206c21d354342100daf645d3b4ca11894a6
29wishes <mwihakinesh2@gmail.com>
2023-05-30 10:27:15 -0700

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - Function thatupdates it value to printer
+ * @n:- single usere input
+ */
+
+ void reset_to_98(int *n)
+{
+	*n = 98;
+}
