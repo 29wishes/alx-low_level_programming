ae2e1c52a51a4987b7f02d7b630964636a1eaf4f
29wishes <mwihakinesh2@gmail.com>
2023-05-30 19:57:35 -0700

    final

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <math.h>
+
+/**
+  * main - entry point.
+  * Return:- Always 0 (success).
+  */
+
+int main(void)
+{
+	char password[84];
+	int index;
+	int total;
+	int diff1;
+	int diff2;
+
+	srand(time(0));
+
+	while (total < 2772)
+	{
+		password[index] = 33 + rand() % 94;
+		total += password[index++];
+
+	}
+	password[index] = '\0';
+
+	if (total != 2772)
+	{
+		diff1 = (total - 2772) / 2;
+		diff2 = (total - 2772) / 2;
+
+		if ((total - 2772) % 2 != 0)
+			diff1++;
+
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff1))
+			{
+				password[index] -= diff1;
+				break;
+			}
+		}
+		for (index = 0; password[index]; index++)
+		{
+			if (password[index] >= (33 + diff2))
+			{
+				password[index] -= diff2;
+				break;
+			}
+		}
+	}
+	printf("%s", password);
+	return (0);
+}
